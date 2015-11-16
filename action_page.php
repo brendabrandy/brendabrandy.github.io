5c04a5af506cf49e82f7dc7ab7c86edeaf5a4ec4
So May Yee Brenda <Brenda_Brandy@Sos-MacBook-Air.local>
2015-11-16 04:31:07 -0500

    fifth commit

--- action_page.php
+++ action_page.php
@@ -2,27 +2,18 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-?>
 
-
-<?php
-    $email_from = '$email';
+   $email_from = '$visitor_email';
+   $email_subject = "New Form submission";
  
-    $email_subject = "New Form submission";
- 
-    $email_body = "You have received a new message from the user $name.\n".
+   $email_body = "You have received a new message from the user $name.\n".
                             "Here is the message:\n $message".
+
+   $to = "sogun3@gmail.com";
+ 
+ 	$headers = "From: $email_from \r\n";
+ 
+  	$headers .= "Reply-To: $visitor_email \r\n";
+ 
+ 	 mail($to,$email_subject,$email_body,$headers);
 ?>
-
-
-<?php
- 
-  $to = "sogun3@gmail.com";
- 
-  $headers = "From: $email_from \r\n";
- 
-  $headers .= "Reply-To: $visitor_email \r\n";
- 
-  mail($to,$email_subject,$email_body,$headers);
- 
- ?>
